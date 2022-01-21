2b749f746c78ee878b66c875643a686df258631d
interactive-game-maps <>
2022-01-21 19:14:31 +0100

    Fix popup location

--- map_utils.js
+++ map_utils.js
@@ -1,9 +1,6 @@
 function getPopupMedia(feature, list_id, html) {
-    const POPUP_WIDTH = 500;
-
     if (feature.properties.image_id) {
         var image_link = document.createElement('a');
-        image_link.className = 'popup-media';
 
         // Check for path
         if (feature.properties.image_id.includes('/')) {
@@ -14,21 +11,11 @@ function getPopupMedia(feature, list_id, html) {
 
         var image = document.createElement('img');
         image.src = image_link.href;
+        image.className = 'popup-media';
         image.height = 300;
 
         image_link.appendChild(image);
         html.appendChild(image_link);
-    } else if (feature.properties.video_id) {
-        var video = document.createElement('iframe');
-        video.className = 'popup-media';
-        video.width = POPUP_WIDTH;
-        video.height = POPUP_WIDTH / 16 * 9;
-        video.src = `https://www.youtube-nocookie.com/embed/${feature.properties.video_id}`;
-        video.title = 'YouTube video player';
-        video.frameborder = 0;
-        // video.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; allowfullscreen'
-
-        html.appendChild(video);
     }
 
     return html;
